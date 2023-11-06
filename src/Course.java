31b7d10170ffeb0729a826ed60d8ca02af251e30
Jacky Tan <gaolin.tan@mail.utoronto.ca>
2023-11-06 18:43:48 -0500

    Created Building code to address reference textfile. Implemented Course and Session entities. Course is a composition of Session.

---
+++ src/Course.java
@@ -0,0 +1,45 @@
+import API.CourseAPI;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Course {
+    private String courseName;
+    private List<Session> lecSessions = new ArrayList<>();
+    private List<Session> tutSessions = new ArrayList<>();
+    private List<Session> praSessions = new ArrayList<>();
+
+
+    public Course(HashMap<String, HashMap<String, ArrayList<HashMap<String,Object>>>> courseInfo){
+        courseName = courseInfo.keySet().iterator().next().toString();
+        for (String sessionCode: courseInfo.get(courseName).keySet()){
+            Session session = new Session(courseInfo.get(courseName).get(sessionCode));
+            session.setSessionCode(sessionCode);
+            String type = sessionCode.substring(0,3);
+            if (type.equals("LEC")){
+                lecSessions.add(session);
+            } else if (type.equals("TUT")) {
+                tutSessions.add(session);
+            }else{
+                praSessions.add(session);
+            }
+        }
+    }
+
+    public boolean hasPra(){
+        /**
+         * return True if there are PRActical sessions for this course
+         */
+        return praSessions.isEmpty();
+    }
+//    Testing
+//    public static void main(String[] args) {
+//        Course course = new Course(CourseAPI.getCourse("CSC207H1 -F"));
+//        System.out.println(course.courseName);
+//        System.out.println(course.lecSessions);
+//        System.out.println(course.tutSessions);
+//        System.out.println(course.praSessions);
+//        System.out.println(course.hasPra());
+//    }
+}
---
+++ src/Session.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * A Session entity that contains the name of the session (LEC0101) and Time and Location.
+ */
+public class Session {
+    private String sessionCode = new String();
+    private List<Integer> startTime = new ArrayList<>();
+    private List<Integer> endTime = new ArrayList<>();
+    private List<Integer> day = new ArrayList<>();
+    private List<String> address = new ArrayList<>();
+    private List<String> buildingCode = new ArrayList<>();
+
+    /**
+     *
+     */
+    public Session(ArrayList<HashMap<String,Object>> session ) {
+        for (HashMap m: session){
+            this.startTime.add((Integer) m.get("Start")/3600000);
+            this.endTime.add((Integer)m.get("endtime")/3600000);
+            this.day.add((Integer) m.get("Day"));
+            this.buildingCode.add((String) m.get("building"));
+            this.address.add(buildingCodeToAddress((String) m.get("building")));
+        }
+
+    }
+    public void setSessionCode(String sessionCode){
+        this.sessionCode = sessionCode;
+    }
+    public String buildingCodeToAddress(String buildingCode){
+        String fileName = "./src/codetoaddress.txt"; // Replace with your file path
+        try {
+            File myObj = new File(fileName);
+            Scanner reader = new Scanner(myObj);
+            String[] parts = new String[0];
+            while (reader.hasNextLine()) {
+                parts = reader.nextLine().split(":");
+                if (parts[0].equals(buildingCode)) {
+                    reader.close();
+                    return parts[1];
+                }
+            }
+            reader.close();
+            throw new RuntimeException();
+        } catch (FileNotFoundException z) {
+            throw new RuntimeException(z);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (sessionCode);
+    }
+}
