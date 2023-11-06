73f4de25bab4e72d9e4e197d3d4aea3a2f95c374
Joshua Jang <musicofmusix@gmail.com>
2023-11-06 17:04:24 -0500

    Update GoogleMapsAPI to accept multiple origins/destinations

--- src/API/GoogleMapsAPI.java
+++ src/API/GoogleMapsAPI.java
@@ -1,62 +1,98 @@
 package org.example;
 
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
-import java.util.Objects;
+import java.util.ArrayList;
 
-public class GoogleMapsAPI
-    {
-    public static void main(String[] args)
-        {
-        String origin = "Bahen Centre";
-        String destination = "Bloor-Yonge Station";
-        String mode = "walking";
+public class GoogleMapsAPI {
+    private ArrayList<String> origins;
+    private ArrayList<String> destinations;
+    private String mode;
 
+    public GoogleMapsAPI(){
+        origins = new ArrayList<String>();
+        destinations = new ArrayList<String>();
+        setMode(0);
+    }
+
+    public void addOrigin(String address) {
+        origins.add(address);
+    }
+
+    public void addDestination(String address) {
+        destinations.add(address);
+    }
+
+    public void clearOriginsDestinations() {
+        origins.clear();
+        destinations.clear();
+    }
+
+    public void setMode(int id) {
+        switch (id) {
+            case 0: // Walking
+                mode = "walking";
+                break;
+            case 1: // Transit
+                mode = "transit";
+                break;
+        }
+    }
+
+    private String encodeLocations(ArrayList<String> locations) {
+        StringBuilder formattedString = new StringBuilder();
+
+        for (int i = 0; i < locations.size(); i++) {
+            formattedString.append(locations.get(i));
+            if (i < locations.size() - 1) formattedString.append(" | ");
+        }
+
+        return formattedString.toString();
+    }
+
+    public void GetDistanceMatrix() {
         final String API_URL =
                 "https://maps.googleapis.com/maps/api/distancematrix/json?origins=%s&destinations=%s&mode=%s&units=metric&key=%s";
-        final String API_KEY = "KEY HERE";
+        final String API_KEY = "AIzaSyA8W-ukR4bacqWfzy4kT0MSn52V-Y_kk0E";
 
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(String.format(API_URL, origin, destination, mode, API_KEY))
+                .url(String.format(API_URL, encodeLocations(origins), encodeLocations(destinations), mode, API_KEY))
                 .build();
-        try
-            {
+        try {
             Response response = client.newCall(request).execute();
             JSONObject responseBody = new JSONObject(response.body().string());
             System.out.println(responseBody);
 
-            if (Objects.equals(responseBody.getString("status"), "OK"))
-                {
-                // The JSON has a lot of layers to unpack..!
-                String resultDestination =
-                        responseBody.getJSONArray("destination_addresses").getString(0);
-                String resultOrigin =
-                        responseBody.getJSONArray("origin_addresses").getString(0);
+            JSONArray resultDestinations =
+                    responseBody.getJSONArray("destination_addresses");
+            JSONArray resultOrigins =
+                    responseBody.getJSONArray("origin_addresses");
 
-                JSONArray rows = responseBody.getJSONArray("rows");
-                JSONObject elements = rows.getJSONObject(0);
+            JSONArray rows = responseBody.getJSONArray("rows");
+
+            for (int i = 0; i < rows.length(); i++) {
+                JSONObject elements = rows.getJSONObject(i);
                 JSONArray elementsArray = elements.getJSONArray("elements");
-                JSONObject firstElement = elementsArray.getJSONObject(0);
-                JSONObject duration = firstElement.getJSONObject("duration");
-                System.out.println(duration);
-                String durationTime = duration.getString("text");
 
-                System.out.printf("From %s to %s:\nTravel time (%s): %s%n",
-                        resultOrigin, resultDestination, mode, durationTime);
-                }
-            else
-                {
-                throw new RuntimeException();
+                for (int j = 0; j < elementsArray.length(); j++) {
+                    JSONObject element = elementsArray.getJSONObject(j);
+                    JSONObject duration = element.getJSONObject("duration");
+                    System.out.println(duration);
+                    String durationTime = duration.getString("text");
+                    System.out.printf("From %s to %s:\nTravel time (%s): %s%n",
+                            resultOrigins.getString(i), resultDestinations.getString(j), mode, durationTime);
                 }
             }
-        catch (IOException | JSONException e)
-            {
+        } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
-            }
         }
     }
+}
