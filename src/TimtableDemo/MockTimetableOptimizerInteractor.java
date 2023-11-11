10d01472fdbb3bb435bfea439d5904619be779ed
Percy Phan <percycat@Percys-MacBook-Air.local>
2023-11-11 12:52:49 -0500

    Add demo package

---
+++ src/TimtableDemo/MockTimetableOptimizerInteractor.java
@@ -0,0 +1,28 @@
+package TimtableDemo;
+
+import entity.Course;
+import use_case.TimeTableOutputData;
+import use_case.TimetableOutputBoundary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockTimetableOptimizerInteractor {
+    private TimetableOutputBoundary outputBoundary;
+
+    public MockTimetableOptimizerInteractor(TimetableOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
+    }
+
+    public void execute() {
+        // Example data
+        List<Course> courses = MockData.getMockCourses();
+        double distance = MockData.getMockTotalDistance();
+        List<Object> mixedList = new ArrayList<>();
+        TimeTableOutputData outputdata = new TimeTableOutputData(courses, distance);
+
+        // Output
+        outputBoundary.presentTimetableOptimizationResults(outputdata);
+    }
+}
+
