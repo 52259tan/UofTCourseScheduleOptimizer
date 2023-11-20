1c178c2d36779c5f92b7d329a61c6414f1c33cca
pinglu <ping.lu@mail.utoronto.ca>
2023-11-20 17:16:08 -0500

    fix bugs in Algorithm, add comments for easier interpreting

--- src/algorithm/Algorithm.java
+++ src/algorithm/Algorithm.java
@@ -4,7 +4,6 @@ import entity.Course;
 import entity.Session;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -37,46 +36,55 @@ public class Algorithm {
      * @return A list of sessions, the final optimal choice Wow!!!
      */
     private static List<Session> getOptimalChoice(List<Course> courses) {
+        // allTImeTable structure below
+        // [
+        // [[sessions for course1], [sessions for course2],[sessions for course3],[sessions for course4], [sessions for course5]]
+        // [[same structure as above]]
+        // [[5 lists of sessions for each courses]]
+        // ...
+        // ]
         List<List<List<Session>>> allTimeTable = getAllTimeTable(courses);
         List<List<Session>> flattenTimeTable = new ArrayList<>();
-        for (List<List<Session>> timeTable : allTimeTable) {
-            flattenTimeTable.add(flattenList(timeTable));
+        for (List<List<Session>> timeTable : allTimeTable) { //sample timeTable: [[sessions for course1], [sessions for course2],[sessions for course3],[sessions for course4], [sessions for course5]]//
+            flattenTimeTable.add(flattenList(timeTable)); //sample element in flattenTimeTable: [sessions for course1, sessions for course2, etc...]
         }
 
         List<Double> allDistances = new ArrayList<>();
-        for (List<Session> timeTable : flattenTimeTable) {
-            List<List<Object>> data1 = new ArrayList<>();
+        for (List<Session> timeTable : flattenTimeTable) { //sample timeTable: [sessions for course1, sessions for course2, etc...]
+            List<List<Object>> data1 = new ArrayList<>(); // data1 stores the list of classes information [startTime, endTime, "location"] the student attend on Monday
             List<List<Object>> data2 = new ArrayList<>();
             List<List<Object>> data3 = new ArrayList<>();
             List<List<Object>> data4 = new ArrayList<>();
             List<List<Object>> data5 = new ArrayList<>();
-            for (Session ses : timeTable) {
-                if (ses.getDay().equals(1)) {
-                    data1.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
-                } else if (ses.getDay().equals(2)) {
-                    data2.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
-                } else if (ses.getDay().equals(23)) {
-                    data3.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
-                } else if (ses.getDay().equals(4)) {
-                    data4.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
-                } else if (ses.getDay().equals(5)) {
-                    data5.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
+            for (Session ses : timeTable) { //sample info in [starTime,endTime,Address] :  [[7,9],[8,10],["location1","location2"]]
+                for (Integer day : ses.getDay()) { //sample days:[1,3] which 1 corresponds to [7,8,"location1"] and 3 corresponds to [9,10,"location2"]
+                    if (day.equals(1)) {
+                        data1.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress())); //sample data in data1 [[7,8,"location1"]]
+                    } else if (day.equals(2)) {
+                        data2.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
+                    } else if (day.equals(3)) {
+                        data3.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
+                    } else if (day.equals(4)) {
+                        data4.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
+                    } else if (day.equals(5)) {
+                        data5.add(List.of(ses.getStartTime(), ses.getEndTime(), ses.getAddress()));
+                    }
                 }
-                // Sort the list based on the start time (index 0)
-                Collections.sort(data1, Comparator.comparingInt(list -> (Integer) list.get(0)));
-                Collections.sort(data2, Comparator.comparingInt(list -> (Integer) list.get(0)));
-                Collections.sort(data3, Comparator.comparingInt(list -> (Integer) list.get(0)));
-                Collections.sort(data4, Comparator.comparingInt(list -> (Integer) list.get(0)));
-                Collections.sort(data5, Comparator.comparingInt(list -> (Integer) list.get(0)));
-                allDistances.add(getDistance(data1) + getDistance(data2) + getDistance(data3) + getDistance(data4) + getDistance(data5));
             }
-        }
-
-
+            // Sort list data based on the startTime of each class, so the classes are in chronological order in each day
+            data1.sort(Comparator.comparingInt(sublist -> (Integer) sublist.get(0))); //sample data1 [[7,8,"location1"], [9,10,"location2"]]
+            data2.sort(Comparator.comparingInt(sublist -> (Integer) sublist.get(0)));
+            data3.sort(Comparator.comparingInt(sublist -> (Integer) sublist.get(0)));
+            data4.sort(Comparator.comparingInt(sublist -> (Integer) sublist.get(0)));
+            data5.sort(Comparator.comparingInt(sublist -> (Integer) sublist.get(0)));
+            // add the total distances of one timeTable, the index of allDistance and flattenTimeTable should represent the same timeTable
+            allDistances.add(getDistance(data1) + getDistance(data2) + getDistance(data3) + getDistance(data4) + getDistance(data5));
+            }
         // Find the index of the minimum distance
         int minIndex = findMinIndex(allDistances);
         return flattenTimeTable.get(minIndex);
-    }
+        }
+
 
     public static Integer findMinIndex(List<Double> allDistances) {
 
