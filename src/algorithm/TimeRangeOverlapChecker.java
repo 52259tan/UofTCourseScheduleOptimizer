7b72da50c0f9264f4e730170da9563ced8f5f050
pinglu <ping.lu@mail.utoronto.ca>
2023-11-20 07:55:48 -0500

    Create a class TimeRangeOverlapChecker and TimeRangeOverlapCheckerDemo to check if two classes in the same day has time conflict

--- src/algorithm/TimeRangeOverlapChecker.java
+++ src/algorithm/TimeRangeOverlapChecker.java
@@ -1,36 +1,32 @@
 package algorithm;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
+/** Check if two sessions have time conflict
+ * @author pinglu
+ */
 public class TimeRangeOverlapChecker {
-    public static void main(String[] args) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-
-            // Define the first time range
-            Date range1Start = sdf.parse("5:00");
-            Date range1End = sdf.parse("7:00");
-
-            // Define the second time range
-            Date range2Start = sdf.parse("6:00");
-            Date range2End = sdf.parse("8:00");
-
-            // Check if the time ranges overlap
-            boolean overlap = doTimeRangesOverlap(range1Start, range1End, range2Start, range2End);
-
-            if (overlap) {
-                System.out.println("The time ranges overlap.");
-            } else {
-                System.out.println("The time ranges do not overlap.");
-            }
-
-        } catch (ParseException e) {
-            e.printStackTrace();
+    /** Check if two sessions have time conflict
+     * @param startTimes the lists of all start times in a session
+     * @param endTimes the lists of all end times in a session
+     * @return A boolean if the two sessions have time conflict
+     */
+    public static boolean checkNoOverlap(List<Integer> startTimes, List<Integer> endTimes) {
+        if (startTimes.size() != endTimes.size()) {
+            throw new IllegalArgumentException("Lists must have the same size.");
         }
-    }
 
-    private static boolean doTimeRangesOverlap(Date start1, Date end1, Date start2, Date end2) {
-        return end1.after(start2) && start1.before(end2);
+        int size = startTimes.size();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                // Check if the current pair does not overlap
+                if (!(endTimes.get(i) < startTimes.get(j)) && !(startTimes.get(i) > endTimes.get(j))) {
+                    return false;
+                }
+            }
+        }
+
+        // No overlap found
+        return true;
     }
 }
