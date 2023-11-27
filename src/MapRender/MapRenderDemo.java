2be8d7d96e30579796705024001c9c6807208634
Joshua Jang <musicofmusix@gmail.com>
2023-11-26 20:14:58 -0500

    Implement MapRenderManager, a higher-level helper class for map rendering
    MapRenderDemo has been updated correspondingly.
    The timetable entity is not completely implemented yet, cannot test.

--- src/MapRender/MapRenderDemo.java
+++ src/MapRender/MapRenderDemo.java
@@ -1,11 +1,8 @@
 package MapRender;
 
-import API.MapRenderAPI;
+import entity.TimeTable;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,30 +11,15 @@ import java.util.List;
  */
 public class MapRenderDemo {
     public static void main(String[] args) {
-        List<String> waypoints = new ArrayList<String>();
+        List<String> waypoints = new ArrayList<>();
         waypoints.add("Bloor-Yonge Station, Toronto");
         waypoints.add("Bahen Centre, Toronto");
         waypoints.add("Dundas Station, Toronto");
 
-        String polyline = MapRenderAPI.getPolyline(waypoints, "walking");
-        BufferedImage image = MapRenderAPI.generateMapWithPolyline(polyline);
-
-        saveImageToFile(image, "map_with_polyline.png");
+        TimeTable timeTable = new TimeTable();
+        MapRenderManager.generateMapsPNG(timeTable);
     }
 
-    private static void saveImageToFile(BufferedImage image, String fileName) {
-        if (image != null) {
-            try {
-                String projectPath = System.getProperty("user.dir");
-                File outputFile = new File(projectPath + File.separator + fileName);
-                ImageIO.write(image, "png", outputFile);
-                System.out.println("Image saved to: " + outputFile.getAbsolutePath());
-            } catch (IOException e) {
-                throw new RuntimeException("Image cannot be saved");
-            }
-        } else {
-            System.out.println("No image to save.");
-        }
-    }
+
 
 }
--- src/MapRender/MapRenderManager.java
+++ src/MapRender/MapRenderManager.java
@@ -1,8 +1,56 @@
 package MapRender;
 
+import API.MapRenderAPI;
+import entity.TimeTable;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /** Higher-level interface for rendering a list of classes into a viewable map image with routes.
  * @author Joshua Jang
  */
 public class MapRenderManager {
+    private static final List<String> days = new ArrayList<>(Arrays.asList("MON", "TUE", "WED", "THU", "FRI"));
+    public static List<BufferedImage> generateMapsBuffer(TimeTable timeTable) {
+        BufferedImage monImg = generateMapImage(timeTable.getAddress1());
+        BufferedImage tueImg = generateMapImage(timeTable.getAddress2());
+        BufferedImage wedImg = generateMapImage(timeTable.getAddress3());
+        BufferedImage thuImg = generateMapImage(timeTable.getAddress4());
+        BufferedImage friImg = generateMapImage(timeTable.getAddress5());
 
+        return new ArrayList<>(Arrays.asList(monImg, tueImg, wedImg, thuImg, friImg));
+    }
+    public static void generateMapsPNG(TimeTable timeTable) {
+        List<BufferedImage> bufferedImages = generateMapsBuffer(timeTable);
+        for(int i = 0; i < 5; i++) {
+            saveImageToFile(bufferedImages.get(i), "/mapimgs/map_" + days.get(i) + ".png");
+        }
+    }
+
+    private static BufferedImage generateMapImage(List<String> addresses) {
+        String polyline = MapRenderAPI.getPolyline(addresses, "walking");
+        BufferedImage image = MapRenderAPI.generateMapWithPolyline(polyline, addresses);
+
+        return image;
+    }
+
+    private static void saveImageToFile(BufferedImage image, String fileName) {
+        if (image != null) {
+            try {
+                String projectPath = System.getProperty("user.dir");
+                File outputFile = new File(projectPath + File.separator + fileName);
+                ImageIO.write(image, "png", outputFile);
+                System.out.println("Image saved to: " + outputFile.getAbsolutePath());
+            } catch (IOException e) {
+                throw new RuntimeException("Image cannot be saved");
+            }
+        } else {
+            System.out.println("No image to save.");
+        }
+    }
 }
