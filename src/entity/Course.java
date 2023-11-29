5710706ee8ce99bbd9e1b36bbe6103898c3d6c98
pinglu <ping.lu@mail.utoronto.ca>
2023-11-28 23:12:12 -0500

    Add a method isValidCourse() to return if the course have at least one valid session (with proper start,end time and buildingCode).

--- src/entity/Course.java
+++ src/entity/Course.java
@@ -1,18 +1,17 @@
 package entity;
 
-import entity.Session;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
 
 public class Course {
     private String courseName;
     private List<Session> lecSessions = new ArrayList<>();
     private List<Session> tutSessions = new ArrayList<>();
     private List<Session> praSessions = new ArrayList<>();
-
+    private boolean isValidCourse = true;
 
     public Course(HashMap<String, HashMap<String, ArrayList<HashMap<String,Object>>>> courseInfo){
         courseName = courseInfo.keySet().iterator().next().toString();
@@ -28,7 +27,10 @@ public class Course {
                 praSessions.add(session);
             }
         }
-    }
+        if (lecSessions.isEmpty()) {isValidCourse=false;}
+        }
+
+
 
     public List<Session> getLecSessions(){
         return lecSessions;
@@ -50,6 +52,8 @@ public class Course {
     public String getCourseName() {
         return courseName;
     }
+
+    public boolean isValidCourse(){return this.isValidCourse;}
 //    Testing
 //    public static void main(String[] args) {
 //        entity.Course course = new entity.Course(CourseAPI.getCourse("CSC207H1 -F"));
