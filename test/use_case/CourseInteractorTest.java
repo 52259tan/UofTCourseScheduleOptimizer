af811802bfafe4513bd38b8a981d40f16501a0a3
bayanmehr <bayan.mehr@mail.utoronto.ca>
2023-12-04 02:57:10 -0500

    incomplete interactor test

--- test/use_case/CourseInteractorTest.java
+++ test/use_case/CourseInteractorTest.java
@@ -4,20 +4,29 @@ import API.CourseAPIContext;
 import algorithm.AlgorithmContext;
 import gui.TimetablePresenter;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import static org.junit.Assert.*;
 
 /**
  * @author bayanmehr
  */
 
 public class CourseInteractorTest {
+    @Test
     public void successTest() {
 
+        CourseInputData courseInputData = new CourseInputData(Arrays.asList("CSC207H1 -F", "CSC236H1 -F"), false);
 
         TimetableOutputBoundary timetablePresenter = new TimetableOutputBoundary() {
             @Override
             public void presentTimetableOptimizationResults(TimeTableOutputData outputData) {
-                System.out.println(outputData);
+                assertNotNull(outputData);
             }
         };
+
+        CourseInteractor courseInteractor = new CourseInteractor(timetablePresenter);
+        courseInteractor.execute(courseInputData);
     }
 }
